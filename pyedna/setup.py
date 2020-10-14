ba826b178198105a72486cd97798672e3124f4f4
Michael Kenney <mfkenney@gmail.com>
2020-10-14 08:49:55 -0700

    Mark package as supplying type hints (PEP 561 compatible)

--- pyedna/setup.py
+++ pyedna/setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 
-setup(name='edna',
-      description='Data acquisition software for the eDNA project',
-      url='https://github.com/sarahewebster/eDNA',
-      author='Michael Kenney',
-      author_email='mikek@apl.uw.edu',
-      license='GPL',
-      packages=['edna', 'edna.apps'],
+setup(name="edna",
+      description="Data acquisition software for the eDNA project",
+      url="https://github.com/sarahewebster/eDNA",
+      author="Michael Kenney",
+      author_email="mikek@apl.uw.edu",
+      license="GPL",
+      packages=["edna", "edna.apps"],
+      package_data={"edna": ["py.typed"],
+                    "edna.apps": ["py.typed"]},
       install_requires=[],
       entry_points = {
       },
