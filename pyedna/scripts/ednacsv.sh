a0016f35917a216d3b66b14b554bd6ddfc889cc3
Michael Kenney <mfkenney@gmail.com>
2020-10-18 08:54:07 -0700

    Add script to convert data to CSV.

---
+++ pyedna/scripts/ednacsv.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Export eDNA data records to CSV. From each eDNA data file, the following
+# CSV files are produced:
+#
+#    - samples_$ID.csv
+#    - results_$ID.csv
+#    - depth_$ID.csv
+#    - battery_$ID.csv
+#
+# This script requires jq (https://stedolan.github.io/jq/) to process the
+# JSON data records.
+#
+
+infile="$1"
+[[ -z $infile ]] && {
+    echo "Missing input file" 2>&1
+    echo "Usage: ${0##*/} infile" 1>&2
+    exit 1
+}
+
+# Extract the deployment ID from the filename
+base="${infile##*/}"
+base="${base%.*}"
+id=$(cut -f2 -d_ <<<"$base")
+
+outfile="sample_${id}.csv"
+echo -n "Creating $outfile ... "
+echo "time,sample,elapsed,amount,pr,depth" > $outfile
+jq -r -M \
+   '{t: .t, ev: (.event / "."), data: .data}|select( .ev[0] == "sample")|[.t, .ev[1], .data.elapsed, .data.amount, .data.pr, .data.depth]|@csv' $infile >> $outfile
+echo "done"
+
+outfile="result_${id}.csv"
+echo -n "Creating $outfile ... "
+echo "time,sample,elapsed,vwater,vethanol,overpressure" > $outfile
+jq -r -M \
+   '{t: .t, ev: (.event / "."), data: .data}|select( .ev[0] == "result")|[.t, .ev[1], .data.elapsed, .data.vwater, .data.vethanol, .data.overpressure]|@csv' $infile >> $outfile
+echo "done"
+
+outfile="depth_${id}.csv"
+echo -n "Creating $outfile ... "
+echo "time,depth" > $outfile
+jq -r -M \
+   'select( .event == "depth")|[.t, .data.depth]|@csv' $infile >> $outfile
+echo "done"
