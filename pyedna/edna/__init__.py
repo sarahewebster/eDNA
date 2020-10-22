fc376d01f44e1425d4c1cab126e0b52c4486cd8b
Michael Kenney <mfkenney@gmail.com>
2020-10-22 10:59:14 -0700

    Add version support to setup script.

--- pyedna/edna/__init__.py
+++ pyedna/edna/__init__.py
@@ -1,4 +1,11 @@
 import time
+try:
+    from importlib import metadata
+except ImportError:
+    # Running on pre-3.8 Python; use importlib-metadata package
+    import importlib_metadata as metadata
+
+__version__ = metadata.version(__name__)
 
 
 def ticker(interval, fsleep=time.sleep):
--- pyedna/setup.py
+++ pyedna/setup.py
@@ -14,14 +14,18 @@ setup(name="edna",
       author_email="mikek@apl.uw.edu",
       license="MIT",
       packages=["edna", "edna.apps"],
-      package_data={"edna": ["py.typed"],
+      package_data={"edna": ["py.typed", "resources/eDNA.cfg"],
                     "edna.apps": ["py.typed"]},
-      install_requires=[],
+      install_requires=[
+          'importlib-metadata ~= 1.0 ; python_version < "3.8"'
+      ],
+      python_requires="~=3.7",
       entry_points = {
           "console_scripts": [
               "runedna=edna.apps.runedna:main",
               "pumptest=edna.apps.pumptest:main",
-              "prtest=edna.apps.prtest:main"
+              "prtest=edna.apps.prtest:main",
+              "installcfg=edna.apps.installcfg:main"
           ]
       },
       zip_safe=False)
