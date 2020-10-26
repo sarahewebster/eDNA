7e6f46dd19f7af5994adb9aef343c8385f4979d1
Michael Kenney <mfkenney@gmail.com>
2020-10-26 15:05:03 -0700

    Add type checking hints.

--- pyedna/edna/__init__.py
+++ pyedna/edna/__init__.py
@@ -1,9 +1,9 @@
 import time
 try:
-    from importlib import metadata
+    from importlib import metadata # type: ignore
 except ImportError:
     # Running on pre-3.8 Python; use importlib-metadata package
-    import importlib_metadata as metadata
+    import importlib_metadata as metadata # type: ignore
 
 __version__ = metadata.version(__name__)
 
